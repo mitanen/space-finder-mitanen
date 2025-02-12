a18a7fd79355dff1b57044c77de279e78dec06b0
Marian M <m3mitanen@gmail.com>
2025-02-12 10:34:52 +0200

    Lecture 46-Delete a record.

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -3,6 +3,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 import { postSpaces } from "./PostSpaces";
 import { getSpaces } from "./GetSpaces";
 import { updateSpace } from "./UpdateSpaces";
+import { deleteSpace } from "./DeleteSpaces";
 
 
 const ddbClient = new DynamoDBClient({});
@@ -24,6 +25,10 @@ async function handler(event: APIGatewayProxyEvent, context: Context): Promise<A
                 const putResponse = await updateSpace(event, ddbClient);
                 console.log(putResponse)
                 return putResponse;
+            case 'DELETE':
+                const deleteResponse = await deleteSpace(event, ddbClient);
+                console.log(deleteResponse)
+                return deleteResponse;
             default:
                 break;
         }
--- test/launcher.ts
+++ test/launcher.ts
@@ -7,12 +7,13 @@ process.env.AWS_REGION = "eu-west-1";
 process.env.TABLE_NAME = 'SpaceTable-068e7469d831'
 
 handler({
-    httpMethod: 'PUT',
+    httpMethod: 'DELETE',
+    //httpMethod: 'PUT',
     //httpMethod: 'GET',
     queryStringParameters: {
         id: '3bb0e645-b435-4217-9d93-ba525185c775'
-    },       
-     body: JSON.stringify({
-         location: 'Dublin2'
-     }) 
+    }//,       
+     //body: JSON.stringify({
+         //location: 'Dublin2'
+     //}) 
 } as any, {} as any);
